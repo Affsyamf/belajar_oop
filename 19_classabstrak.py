c523c447b77590edeb447e7e7b8dacdd9174b707
Afif Syam Fauzi <afiffauzi2016@gmail.com>
2025-11-17 17:57:31 +0700

    asbtrak class adalah blueprint untuk class

---
+++ 19_classabstrak.py
@@ -0,0 +1,24 @@
+# abstrak class
+# abc = abstract base class
+from abc import ABC, abstractmethod
+
+class Button(ABC):
+    
+    @abstractmethod
+    def click(self):
+        pass
+        
+class PushButton(Button):
+    
+    def click(self):
+        print("push button click")
+        
+class RadioButton(Button):
+    
+    def click(self):
+        print("button radio")
+        
+tombol1 = PushButton()        
+tombol2 = RadioButton()        
+tombol1.click()
+tombol2.click()
