b347746798e867b786fef3497445c92814d81f53
Afif Syam Fauzi <afiffauzi2016@gmail.com>
2025-11-17 18:08:24 +0700

    video 20 menggabungkan class abstrak dan decorator

---
+++ 20_absdec.py
@@ -0,0 +1,39 @@
+# abstrak class dan decorator method
+
+from abc import ABC, abstractmethod
+
+class Button(ABC):
+    
+    def __init__(self,set_link):
+        self.link = set_link
+        
+    
+    @abstractmethod
+    def click(self):
+       pass
+   
+    @property
+    @abstractmethod
+    def link(self):
+        pass
+       
+
+class PushButton(Button):
+    
+    def click(self):
+        print("Go to: {}".format(self.link))
+        
+        
+    @Button.link.setter
+    def link(self, input):
+        self.__link = input
+        
+    @link.getter
+    def link(self):
+        return self.__link
+        
+        
+        
+        
+tombol1 = PushButton("https://portofolio.affsyamf.my.id/web porto")
+tombol1.click()
