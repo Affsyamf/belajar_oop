4b6c89ca731c055f0e53abb734edc2642fc97018
Afif Syam Fauzi <afiffauzi2016@gmail.com>
2025-11-17 16:58:57 +0700

    latihan inheritence

---
+++ 14_latihan3.py
@@ -0,0 +1,15 @@
+# latihan iheritence
+    
+from hero import Hero_intel, Hero_streng
+
+dira = Hero_intel('dira')
+amira = Hero_streng('amira')
+
+dira.show_info()
+amira.show_info()
+
+dira.gainExp = 200
+amira.gainExp = 300
+
+dira.show_info()
+amira.show_info()
